e23104dfc3964d09cd0d6c4f12a7c5630744c531
KrittinKlinkaew63070004 <73655235+KrittinKlinkaew63070004@users.noreply.github.com>
2020-11-13 10:16:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
     <h3>Work2 / Work3 06/11/2020</h3>
     <a href="wwwww/index2.html">work2</a><br>
     <a href="wwwww/index3.html">work3</a>
+    <a href="wwwww/show2.php">work4</a>
 </body>
 </html>
